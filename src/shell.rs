f2cdb4300a7ba7aed8a90359397bf70a6114b72d
ademnem <adamnam012@gmail.com>
2025-05-23 21:37:32 -0700

    shell no longer makes strings uppercase

--- src/shell.rs
+++ src/shell.rs
@@ -1,45 +1,36 @@
 use std::io::{stdin, stdout, Write};
-use std::string::String;
 use std::result::Result;
-
-
+use std::string::String;
 
 pub fn pure_lisp_interpreter_message() {
     println!("--== Pure Lisp Interpreter ==--");
 }
 
+fn paren_balance(command: &str) -> i16 {
+    let mut bal: i16 = 0;
 
-
-fn paren_balance(command: &str) -> i16{
-
-    let mut bal: i16 = 0; 
-    
     for c in command.chars() {
         match c {
             '(' => bal += 1,
             ')' => bal -= 1,
-            _ => {},
+            _ => {}
         }
     }
 
     bal
 }
 fn rparen_is_last(input: &String) -> bool {
-
-    // if there are inputs before the first (, only the first argument will be evaluated 
+    // if there are inputs before the first (, only the first argument will be evaluated
     if input.starts_with('(') {
         match input.chars().last() {
             Some(c) => return c == ')',
             None => return false,
-        }    
+        }
     }
     true
 }
 
-
-
-fn get_command_new_line() -> String { 
-
+fn get_command_new_line() -> String {
     let mut line = String::new();
     print!("> ");
     let _ = stdout().flush();
@@ -48,20 +39,18 @@ fn get_command_new_line() -> String {
     line
 }
 fn get_command_line() -> String {
-
     let mut line = String::new();
     stdin().read_line(&mut line).expect("unable to read");
 
     line
 }
 pub fn get_command() -> Result<String, String> {
-
-    let mut input = String::new(); 
+    let mut input = String::new();
 
     input += &get_command_new_line();
     let _ = input.pop();
     while paren_balance(&input) > 0 {
-        input += &get_command_line(); 
+        input += &get_command_line();
         let _ = input.pop();
     }
 
@@ -73,10 +62,8 @@ pub fn get_command() -> Result<String, String> {
         return Err(String::from("input starting with a '(' must end with ')'"));
     }
 
-    Ok(input.to_uppercase())
+    Ok(input)
 }
-
-
 
 #[cfg(test)]
 mod tests {
@@ -84,26 +71,24 @@ mod tests {
 
     #[test]
     fn test_paren_balance() {
-
         let mut command = String::new();
-        assert_eq!(paren_balance(&command), 0);  
+        assert_eq!(paren_balance(&command), 0);
 
         command = String::from("(");
-        assert_eq!(paren_balance(&command), 1);  
+        assert_eq!(paren_balance(&command), 1);
 
         command = String::from(")");
-        assert_eq!(paren_balance(&command), -1);  
+        assert_eq!(paren_balance(&command), -1);
 
         command = String::from("()");
-        assert_eq!(paren_balance(&command), 0);  
+        assert_eq!(paren_balance(&command), 0);
 
         command = String::from("(hello world)");
-        assert_eq!(paren_balance(&command), 0);  
+        assert_eq!(paren_balance(&command), 0);
     }
 
     #[test]
     fn test_rparen_is_last() {
-
         let mut input = String::new();
         assert!(rparen_is_last(&input));
 
@@ -120,4 +105,3 @@ mod tests {
         assert!(!rparen_is_last(&input));
     }
 }
-
